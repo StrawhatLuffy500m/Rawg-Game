ec5f7a45e86adb04b0bdf8a4400e4444f3713901
Arya <aryavaghela111@gmail.com>
2024-09-17 14:57:56 +0530

    Creating NavBar and changing Theme

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import React from "react";
+import ColorModeSwitch from "./colorModeSwitch";
+
+const NavBar = () => {
+  return (
+    <HStack justifyContent="space-between" padding='10px'>
+      <Image
+        src={logo}
+        boxSize="60px"
+      ></Image>
+      <ColorModeSwitch></ColorModeSwitch>
+    </HStack>
+  );
+};
+
+export default NavBar;
