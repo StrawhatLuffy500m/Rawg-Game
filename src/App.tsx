326bed19310389d43e8d07f5e46c7fb0ed2e2cee
Arya <aryavaghela111@gmail.com>
2024-09-19 15:56:49 +0530

    Displaying Genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr'
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside"><GenreList /></GridItem>
+        <GridItem area="aside" padding={3}><GenreList /></GridItem>
       </Show>
 
       <GridItem area="main">
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,18 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useGenre, { Genre } from '../hooks/useGenre'
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
     const {data} = useGenre();
     return (
-    <ul>
-        {data.map(genres => <li key={genres.id}>{genres.name}</li>)}
-    </ul>
+    <List>
+        {data.map(genres => <ListItem key={genres.id} paddingY='5px'>
+          <HStack>
+          <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genres.image_background)}></Image>
+          <Text fontSize='lg'>{genres.name}</Text>
+          </HStack>
+        </ListItem>)}
+    </List>
   )
 }
 
